f5b3c105c49f35460781c567f09e76a2e0846a8d
bs287 <bruna-m.silva@hotmail.com>
2025-02-26 15:44:33 -0300

    Correção do nome da classe de teste.

--- code-challenge-migration/src/test/java/com/example/dummyjson/service/ProductServiceTest.java
+++ code-challenge-migration/src/test/java/com/example/dummyjson/service/ProductServiceTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-class ProductControllerTest {
+class ProductServiceTest {
 
     @Autowired
     public WebTestClient webTestClient;
